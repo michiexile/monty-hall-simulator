d17f8bed34dd254aaeebd51f97e3715d441d5976
michiexile <michiexile@users.noreply.github.com>
2022-10-22 03:19:29 +0000

    deploy: 2aae79445a39a8f65b3111e778b0c35f987db810

---
+++ assets/QCardSection.0c17d018.js
@@ -0,0 +1,1 @@
+import{c as r,h as e}from"./QBtn.dfb88130.js";import{a as n,h as s}from"./index.81160f9d.js";var l=r({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(t,{slots:a}){const o=n(()=>`q-card__section q-card__section--${t.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>s(t.tag,{class:o.value},e(a.default))}});export{l as Q};
